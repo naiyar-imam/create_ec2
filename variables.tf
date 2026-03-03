2351a4887660f89ab03fac473593b54b3e122ee6
naiyar-imam <naiyar.imam@bellblazetech.com>
2026-03-03 11:57:20 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    default = "ap-south-1"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "ami_id" {
+    description = "Amazon Linux 2 AMI "
+  
+}
+
